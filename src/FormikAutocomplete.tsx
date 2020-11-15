b26db7f7088ee47568c75ea02ebe4fd04cc03ac8
Chris <fuleinist@gmail.com>
2020-11-15 18:23:46 +1100

    add overwrite props to textFieldProps in Autocomplete

--- src/FormikAutocomplete.tsx
+++ src/FormikAutocomplete.tsx
@@ -51,6 +51,7 @@ export function FormikAutocomplete(props: FormikAutocompleteProps) {
               error: hasError(name, form, error),
               helperText: getHelperText(name, form, helperText),
               label
+              {...others}
             }}
             {...others}
           />
